81f42ac526a2e0cb97e09646954895cbeddf1c0c
shalin <shalin.rono@student.moringaschool.com>
2021-09-18 19:28:33 +0300

    enable sender email

--- app/email.py
+++ app/email.py
@@ -1,3 +1,11 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
+
+def mail_message(subject, template, to, **shal):
+    sender_email = 'shalin.rono@student.moringaschool.com'
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body = render_template(template + '.txt', **shal)
+    email.html = render_template(template + '.html', **shal)
+    mail.send(email)
