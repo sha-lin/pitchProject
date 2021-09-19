48607c6f891d51a500c4af9963fbdc9d2b268892
shalin <shalin.rono@student.moringaschool.com>
2021-09-19 17:45:34 +0300

    enable log out of user

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,3 +22,31 @@ def login():
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
+# route register
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+         author = form.author.data,
+         password = form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitch-App!","email/welcome_user",user.email,user=user)
+
+        title = "New Account"
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html',registration_form = form)
+
+    # route logout
+    
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
