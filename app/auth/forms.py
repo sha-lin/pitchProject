23d2f8a9f8ce1083face97fee4ba0cf8d4961472
shalin <shalin.rono@student.moringaschool.com>
2021-09-19 17:38:58 +0300

    add registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    author = StringField('Enter authors name',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_author(self,data_field):
+        if User.query.filter_by(author = data_field.data).first():
+            raise ValidationError('That author name is taken')
